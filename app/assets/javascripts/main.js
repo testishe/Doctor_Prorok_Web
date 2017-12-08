92c89e2338d1a92fe65e424cd740b5e070813540
testishe <32316231+testishe@users.noreply.github.com>
2017-12-08 16:53:27 +0500

    page index

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	$(".slider").owlCarousel({
+	items: 1,
+	nav: true,
+	navText: [ , ],
+	dots: false,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	autoplayHoverPause: true,
+	loop: true,
+	mouseDrag: true
+	});
+
+});
